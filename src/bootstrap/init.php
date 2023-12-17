e0529f3b2e4cfde8fddd21665ad4957b6c014bd9
gsh519 <gsh05196yu@yahoo.co.jp>
2023-12-17 14:44:51 +0900

    initファイル作成

---
+++ src/bootstrap/init.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'mysql:host=mysql;dbname=ad5_lesson;charset=utf8mb4';
+$user = 'root';
+$password = 'password';
+
+// PDO接続
+try {
+    $pdo = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    // エラーログに記録
+    exit($e->getMessage());
+}
