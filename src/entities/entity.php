8722eee8180dd15adca4422af91b52c0810e58ab
gsh519 <gsh05196yu@yahoo.co.jp>
2023-12-17 14:45:28 +0900

    entityの親クラス作成

---
+++ src/entities/entity.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Entity
+{
+    public function __get(string $key): mixed
+    {
+        // アクセサ
+        $pascal_key = str_replace(' ', '', ucwords(str_replace('_', ' ', $key)));
+        $method_name = 'get' . $pascal_key . 'Attribute';
+        if (method_exists($this, $method_name)) {
+            return $this->{$method_name}();
+        }
+
+        return null;
+    }
+}
