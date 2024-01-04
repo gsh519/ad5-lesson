498b52e623efe65af5416fd4f8f6a57daf8fd688
gsh519 <gsh05196yu@yahoo.co.jp>
2024-01-04 23:10:30 +0900

    html head箇所 共通化

---
+++ src/resources/views/header.view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD5 Lesson</title>
+    <link rel="stylesheet" href="./resources/css/reset.css">
+    <link rel="stylesheet" href="./resources/css/common.css">
+    <link rel="stylesheet" href="./resources/css/style.css">
+</head>
