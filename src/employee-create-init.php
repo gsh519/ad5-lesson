0e11ab852d9a12e4ce64029f061379421f6b28c9
gsh519 <gsh05196yu@yahoo.co.jp>
2024-02-04 12:01:35 +0900

    ファイル名修正

--- src/employee-create-init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require(__DIR__ . '/controllers/EmployeeCreateInitController.php');
-
-$controller = new EmployeeCreateInitController();
-$controller->addInit();
