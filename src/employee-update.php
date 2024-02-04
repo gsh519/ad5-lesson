6383b14a7e514277a3d1e3f20d71cd95f82a0d8e
gsh519 <gsh05196yu@yahoo.co.jp>
2024-02-04 12:02:16 +0900

    $_POSTによって処理メソッド分ける

---
+++ src/employee-update.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+require_once(__DIR__ . '/controllers/Employee/UpdateController.php');
+
+$controller = new UpdateController();
+if (!empty($_POST)) {
+    $controller->update();
+    return;
+}
+
+$controller->updateInit();
